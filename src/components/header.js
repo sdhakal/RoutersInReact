2a9906feb5be420543d6e383eb1d921ac8c2551e
Suman Dhakal <suman007dhakal@gmail.com>
2019-04-06 13:05:56 +0545

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/styles.css";
+
+const Header = props => {
+  return (
+    <header>
+      <div className="logo">Logo</div>
+      <input type="text" onChange={props.keywords} />
+    </header>
+  );
+};
+
+export default Header;
